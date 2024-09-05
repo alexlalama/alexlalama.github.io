da4540160c49582b363c015aa561f9e71a4d1921
alexlalama <alexlalamaemail@gmail.com>
2024-09-05 08:03:42 -0400

    new index.html

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+// create element Ul, create li element, add class.course,
+
+{/* <ul class="nav">
+        <li class="home">
+            <a href="index.html">Home</a>
+        </li>
+        <li class="home">
+            <a href="index.html">Projects</a>
+        </li>
+        <li class="dropdown">
+            <a href="#">Production</a>
+            <!-- <ul class="dropdown-ul">
+                    <li><a href="#">Music</a></li>
+                    <li><a href="#">Photo</a></li>
+                    <li><a href="#">Video</a></li>
+                    <li><a href="#">Mix, DJ</a></li>
+            </ul> -->
+        </li>
+        <li>
+            <a href="contact.html">Contact</a>
+        </li>
+    </ul> */}
+
+    var ul = document.createElement('ul')// cry i took the top off
+    ul.className("nav")
+    function ul(poop){
+        // loop 
+        poop = 0
+        while(poop>4)0
+        {
+            li = document.createElement('li')
+            poop +=1
+            
+  
+        }
+
+    }
+    navUl(4)
