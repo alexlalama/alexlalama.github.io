060fe96a8bba2e34838ca4d4e285fc68a3716277
alexlalama <alexlalamaemail@gmail.com>
2025-06-30 23:44:02 -0400

    slide show for loop

---
+++ assets/js/about.js
@@ -0,0 +1,19 @@
+
+var slideShow = [];
+var i= 0;
+function slides(){
+    for(i in slideShow)
+    {
+        slideShow[i] = image;
+        i++;
+        alert("slides")
+    }
+}
+function nextImage(){
+    
+
+}
+
+function previousImage(){
+
+}
