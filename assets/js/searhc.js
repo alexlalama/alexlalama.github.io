1ffab5b1b116f0355fa26242fa0e7f89d7d3368d
alexlalama <alexlalamaemail@gmail.com>
2025-07-18 16:15:44 -0400

    search js

---
+++ assets/js/searhc.js
@@ -0,0 +1,23 @@
+row = 0
+for(row = 0; row <= 3; row++){
+    letter = document.createElementByTagName("p");
+    holder = document.getElementById("search-holder");
+    holder.append(letter);
+    //letter.className(''); // class name
+    /*
+    for(col = 0; col <= 3; col++){
+        letter = document.createElementByTagName("p");
+        letter.className(''); // class name 
+
+        }
+    }
+file = open("./../words.txt");
+    for(word in file){
+        for(letter in each word){
+        letter.innerHTML = letter;
+
+        }
+    }
+    
+*/
+}
