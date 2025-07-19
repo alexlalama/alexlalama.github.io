335faa5638e6cb4f9eebad193dee3f6a29c8915b
alexlalama <alexlalamaemail@gmail.com>
2025-07-19 00:31:45 -0400

    function

--- assets/js/searhc.js
+++ assets/js/searhc.js
@@ -5,25 +5,24 @@ var button_four = document.getElementById('');
 var colletter;
 
 var ranWord = Math.floor(Math.random() * 5);
-function generateFour(){
-    /* */
-
-}
-
-for(let row = 0; row < 3; row++){
-    var letter = document.createElement('p');
-    var holder = document.getElementById("search-holder");
-    holder.append(letter);
-    letter.innerHTML = 'ls';
-    letter.classList.add("letters"); // class name
-    for(let col = 0; col < 3; col++){
-        colletter = document.createElement('p');
-        letter.append(colletter);
-        colletter.classList.add("letters"); // class name 
-        colletter.innerHTML = 'col';
-
+function generate_four(){
+        
+    
+    for(let row = 0; row < 3; row++){
+        var letter = document.createElement('p');
+        var holder = document.getElementById("search-holder");
+        holder.append(letter);
+        letter.innerHTML = 'ls';
+        letter.classList.add("letters"); // class name
+        for(let col = 0; col < 3; col++){
+            colletter = document.createElement('p');
+            letter.append(colletter);
+            colletter.classList.add("letters"); // class name 
+            colletter.innerHTML = 'col';
         }
 }
+}
+
 
     // if  button selected, choose random word with number 
 /*file = open("./../words.txt");
