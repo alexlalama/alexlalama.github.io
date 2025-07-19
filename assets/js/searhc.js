b719fd7302c84d3e6632a2c98a8d6d542b99f83e
alexlalama <alexlalamaemail@gmail.com>
2025-07-19 00:45:21 -0400

    div element

--- assets/js/searhc.js
+++ assets/js/searhc.js
@@ -7,13 +7,13 @@ var ranWord = Math.floor(Math.random() * 5);
 
 function generate_four(){
     for(let row = 0; row < 3; row++){
-        var letter = document.createElement('p');
+        var letter = document.createElement('div');
         var holder = document.getElementById("search-holder");
         holder.append(letter);
         letter.innerHTML = 'ls';
         letter.classList.add("letters"); // class name
         for(let col = 0; col < 3; col++){
-            colletter = document.createElement('p');
+            colletter = document.createElement('div');
             // colletter.classList.add("letters"); // class name 
             colletter.innerHTML = 'col';
         }
