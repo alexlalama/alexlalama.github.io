d49afb61f7246578acf96529219a00c794367352
alexlalama <alexlalamaemail@gmail.com>
2025-07-19 00:14:41 -0400

    =

--- assets/js/searhc.js
+++ assets/js/searhc.js
@@ -1,16 +1,16 @@
 console.clear();
 var row;
 var col;
-for(let row = 0; row <= 3; row++){
+for(let row = 0; row < 3; row++){
     var letter = document.createElement('p');
     var holder = document.getElementById("search-holder");
     holder.append(letter);
     letter.classList.add("letters"); // class name
-    for(let col = 0; col <= 3; col++){
+    for(let col = 0; col < 3; col++){
         letter = document.createElement('p');
         letter.classList.add("letters"); // class name 
         letter.innerText = 'letters';
-        console.log("log")
+        console.log("log");
         }
 }
 
