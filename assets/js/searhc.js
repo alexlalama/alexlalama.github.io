52b210d53f23a05489165261e89f0ba876b9df1f
alexlalama <alexlalamaemail@gmail.com>
2025-07-19 00:13:18 -0400

    html

--- assets/js/searhc.js
+++ assets/js/searhc.js
@@ -6,13 +6,13 @@ for(let row = 0; row <= 3; row++){
     var holder = document.getElementById("search-holder");
     holder.append(letter);
     letter.classList.add("letters"); // class name
-
     for(let col = 0; col <= 3; col++){
         letter = document.createElement('p');
         letter.classList.add("letters"); // class name 
         letter.innerText = 'letters';
+        console.log("log")
         }
-    }
+}
 
     // if  button selected, choose random word with number 
 /*file = open("./../words.txt");
