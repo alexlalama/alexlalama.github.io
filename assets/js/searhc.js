00d61ee9e2d2837dee0041543ee5f23b7d0640b5
alexlalama <alexlalamaemail@gmail.com>
2025-07-18 23:27:41 -0400

    create element

--- assets/js/searhc.js
+++ assets/js/searhc.js
@@ -8,7 +8,7 @@ for(let row = 0; row <= 3; row++){
     letter.classList.add("letters"); // class name
 
     for(let col = 0; col <= 3; col++){
-        letter = document.createElementByTagName("p");
+        letter = document.createElement('p');
         letter.classList.add("letters") // class name 
         letter.innerText = 'letters'
         }
