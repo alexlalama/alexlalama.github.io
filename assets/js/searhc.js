462d9dcd81c835a8f169c3e2ee772367fc318991
alexlalama <alexlalamaemail@gmail.com>
2025-07-18 23:09:01 -0400

    id

--- assets/js/searhc.js
+++ assets/js/searhc.js
@@ -1,7 +1,7 @@
 var row;
 for(let row = 0; row <= 3; row++){
     var letter = document.createElement("p");
-    var holder = document.getElementById("#search-holder");
+    var holder = document.getElementById("search-holder");
     holder.append(letter);
     //letter.className(''); // class name
     /*
