0ea7cffb9350efb00fa56660c1962938c4bbf28b
alexlalama <alexlalamaemail@gmail.com>
2025-08-04 18:46:07 -0400

    number

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -22,7 +22,7 @@ var six = buttonVar("button6");
 var seven = buttonVar("button7");
 var eight = buttonVar("button8");
 var nine = buttonVar("button9");
-var calculatorNumber = document.querySelector("p#calculator-number").innerText = "0"; // Calculator number holder
+var calculatorNumber = document.querySelector("p#calculator-number") ="0"; // Calculator number holder
 
  // var clear = document.querySelector("button#clear"); // Clear button
 Number(calculatorNumber); // parse string into number
