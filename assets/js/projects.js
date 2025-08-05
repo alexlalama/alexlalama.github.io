24b7c48c7b70c79d77835738a1ce06ceecf9603a
alexlalama <alexlalamaemail@gmail.com>
2025-08-05 16:17:58 -0400

    onclick plus

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,49 +1,39 @@
 console.clear();
-// const arrayOfButtons = [zero, one, two, three, four, five, six, seven, eight, nine];
 
+var calculatorNumber = document.getElementById("calculator-number");  // Calculator number holder
+var equals = document.getElementById("equals"); 
+var plus = document.getElementById("plus");
+var divide = document.getElementById("divide"); 
+var minus = document.getElementById("minus");
+var number = 0;
+var currentNumber = 0;
 function buttonVar(name) // Select HTML element
 {
-  return document.querySelector(`button#${name}`);
+  return document.querySelector(`#button${name}`);
 }
-var zero = buttonVar("button0");
-var one = buttonVar("button1");
-var two = buttonVar("button2");
-var three = buttonVar("button3");
-var four = buttonVar("button4");
-var five = buttonVar("button5");
-var six = buttonVar("button6");
-var seven = buttonVar("button7");
-var eight = buttonVar("button8");
-var nine = buttonVar("button9");
 
-var calculatorNumber = document.getElementById("p#calculator-number").innerHTML;  // Calculator number holder
-calculatorNumber.innerHTML = Number("0");
-console.log(calculatorNumber.innerHTML);
 function clearFunction() // Clear calculator number
 {
-  calculatorNumber.innerHTML= "0";  
-  Number(calculatorNumber);
-  console.log(3);
-  return 0;
+  calculatorNumber.innerHTML = 0;  
 }
-// var answered = false;
-/*while(!answered)
-{
-  for(let i = 0; i <= arrayOfButtons.length-1; i++)
-    {
-      parseInt(arrayOfButtons[i]);
-      console.log(arrayOfButtons[i]);
-      answered= true;
-      arrayOfButtons[i].addEventListener("click",function(){
-          calculatorNumber = arrayOfButtons[i];
-          console.log(calculatorNumber);
-      })
-    }
+
+
+function equals(){
+    return parseInt(calculatorNumber.innerHTML);
 }
-*/
-function add()
-{
-  var sum =0;
-  sum += calculatorNumber;
-  return sum;
+var currentNumber = 0;
+var number = 0;
+var input = 0;
+for(let i=0; i <= 9; i++)
+  {
+    buttonVar(i).addEventListener("click", function(){
+    currentNumber = parseInt(buttonVar(i).innerHTML);
+    return currentNumber;
+    });
+  }
+
+function add(){
+  number += cutterNumber;
+  console.log("4");
+  return number; 
 }
