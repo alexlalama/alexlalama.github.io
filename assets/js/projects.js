2a94e6d0b4c58c6378a1b4346ef0190e209da417
alexlalama <alexlalamaemail@gmail.com>
2025-10-20 17:43:08 -0400

    commented out code

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,39 +1,43 @@
-console.clear();
+function isAlpha(inputValue, input)
+{   
+    if(inputValue == "")
+    {
+        input.focus();
+        alert(input.placeholder +" must be numbers");
+        return 0;
+    }else if(isNaN(inputValue) == true )
+    {
+        input.focus();
+        alert(input.placeholder +" must only be numbers");
+        return 0;
+    }
+    else{
+        return parseInt(inputValue);
+    }
+}
+const subtotal = document.querySelector("#subtotalInput");
+const rate = document.querySelector("#percentInput");
+let total = document.querySelector("#total");
 
-var calculatorNumber = document.getElementById("calculator-number");  // Calculator number holder
-var equals = document.getElementById("equals"); 
-var plus = document.getElementById("plus");
-var divide = document.getElementById("divide"); 
-var minus = document.getElementById("minus");
-var number = 0;
-var currentNumber = 0;
-function buttonVar(name) // Select HTML element
+function calculate()
 {
-  return document.querySelector(`#button${name}`);
-}
+   
 
-function clearFunction() // Clear calculator number
+    let subtotalValue = subtotal.value; 
+    let rateValue = rate.value;
+    let newSubtotal, newRate;
+
+    rateValue = rateValue/100;
+    rateValue = subtotalValue * rateValue;
+    newSubtotal = isAlpha(subtotalValue, subtotal);
+    newRate = isAlpha(rate.value, rate);
+    total.innerHTML = newRate + newSubtotal;
+   
+}
+function clearCalculator()
 {
-  calculatorNumber.innerHTML = 0;  
+    subtotal.value = 0;
+    rate.value = 0;  
+    total.innerHTML = 0;
 }
 
-
-function equals(){
-    return parseInt(calculatorNumber.innerHTML);
-}
-var currentNumber = 0;
-var number = 0;
-var input = 0;
-for(let i=0; i <= 9; i++)
-  {
-    buttonVar(i).addEventListener("click", function(){
-    currentNumber = parseInt(buttonVar(i).innerHTML);
-    return currentNumber;
-    });
-  }
-
-function add(){
-  number += cutterNumber;
-  console.log("4");
-  return number; 
-}
