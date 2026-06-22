f9d23320326e803ae671566cb6f18739a739b340
alexlalama <alexlalamaemail@gmail.com>
2026-06-22 18:27:17 -0400

    new things

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,8 +1,64 @@
-var i= 0;
-var arr = []
+const exp = {
+    "dollarTree" : 
+        {"position":"Customer Associate",
+        "company":"Dollar Tree",
+        "duration":"Jan 2026 - Present"},
+   
+    "Hallmark" : 
+        {"position":" Sales Retail Associate",
+        "company":"Dollar Tree",
+        "duration":" Aug 2025 - Jan 2026"},
 
-var image = document.querySelector("img#image1");
+    "Anthony's Coal Fired Pizza":
+        {"position":"Dishwasher - Anthony's Coal Fired Pizza Aug 2025 - ",
+        "company":"Dollar Tree",
+        "duration":"Jan 2026 - Present"},
 
-image.style.width= '100px';
-console.log(image.style);
+    "Rutgers University's Office of Registrar":
+        {"position":"Student Worker - Rutgers University's Office of Registrar May 2024 - Aug 2024",
+        "company":"Dollar Tree",
+        "duration":"Jan 2026 - Present"},
 
+    "Bowlero Corporation":
+        {"position":"Cook - Bowlero Corporation May 2024 - Aug 2024",
+        "company":"Dollar Tree",
+        "duration":"Jan 2026 - Present"},
+
+    "Staples":
+        {"position":"Retail Sales Print Associate - Staples May 2023 - Aug 2023",
+        "company":"Dollar Tree",
+        "duration":"Jan 2026 - Present"},
+
+    "Whole Foods Market":
+        {"position":"In-Store Shopper",
+        "company":"Dollar Tree",
+        "duration":"Jan 2026 - Present"},
+
+    "Chick-Fil-A":
+        {"position":"Front of House Team Member",
+        "company":"Dollar Tree",
+        "duration":"Jan 2026 - Present"},
+}
+
+
+// locate section of jobs
+// create element for job 
+// create element for position
+// create element for company
+// create element for duration
+
+
+var positionEle = document.createElement("div");
+
+
+
+
+
+
+experienceSection.append(positionEle);
+positionEle.className= "position";
+positionEle.innerHTML= "Dollar Tree";
+console.log(experienceSection);
+
+document.body.append(experienceEle);
+document.body.append(experienceEle);
